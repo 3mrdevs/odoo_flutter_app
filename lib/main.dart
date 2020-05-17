11825b3dd90ce4d0365a4bfa3e43d7a489982a69
Amr Abd-Alkrim Hassan <wizmoori@gmail.com>
2020-05-18 00:23:03 +0200

    Change title

--- lib/main.dart
+++ lib/main.dart
@@ -8,12 +8,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter Odoo Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'Flutter Odoo Demo Home Page'),
     );
   }
 }
